fc44c31ad1ff63a37721a2d140fb8330ace77a81
igavelyuk <gavelyukworking@gmail.com>
2020-02-18 14:53:40 -0600

    Spin, adding pages about, adding store for posts

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Successful youtuber blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || 'Top Ukraine gamer youtuber, RDR2, Grand Theft Auto 5' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="about">
+    <h1>About page</h1>
+    <p>Some info</p>
+  </div>
+</template>
+
+<script>
+// import OtherComponent from './OtherComponent.vue'
+export default {
+  components: {
+    // OtherComponent
+  },
+  data () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
