b8efd95d91733f668056abd10e1cc50865b2785d
igavelyuk <gavelyukworking@gmail.com>
2020-02-19 12:00:50 -0600

    Nav menu with logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="logo">
-    logo
+    <img class="logo" src="@/assets/21281697.png" alt="">
   </div>
 </template>
+<script>
+// import Image from '~/assets/21281697.png'
+//
+// export default {
+//   components: {
+//     Image
+//   },
+//   data () {
+//     return {
+//       greeting: 'Hello'
+//     }
+//   }
+// }
+</script>
 <style>
 .logo{
-  display: grid;
-  background-color: magenta;
-  height: 200px;
-  width: 200px;
+  height: 100px;
+  width: 100px;
+  vertical-align: middle;
 }
 </style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,15 @@
 <template lang="html">
   <div class="navbar">
       <div class="logoitem">
-        <Logo/>
+        <a href="">
+          <Logo/>
+        </a>
       </div>
       <div class="menuitems">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Videos</a></li>
-          <li><a href="#">Merch</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
+          <a class="btn btn-4" href="#"><span> Home </span></a>
+          <a class="btn btn-4" href="#"><span> Videos </span></a>
+          <a class="btn btn-4" href="#"><span>Merch</span></a>
+          <a class="btn btn-4" href="#"><span>About</span></a>
       </div>
   </div>
 </template>
@@ -30,36 +30,107 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
-*{
-  outline: 1px solid red;
-}
+<style lang="scss" scoped>
 .navbar{
-  /* display: grid; */
-  background-color: lightskyblue;
-  height: 200px;
-  /* grid-template-columns: 20% 20% 20% 20% 20%; */
-}
-.logoitem{
-  /* display: grid; */
-}
-.menuitem{
   display: grid;
-  /* grid-column-start :4;
-  grid-column-end: 6; */
-  grid-template-columns: 20% 20% 20% 20% 20%;
-  grid-area: 1;
-  border: 1px solid black;
-}
-li{
-  /* display: grid; */
-  grid-column-start :2;
-  /* grid-column-end: 5; */
-  /* grid-area: 1 / 2 / 1 /6; */
-  vertical-align: center;
-  display:inline-block;
-  font-size: 30px;
-  font-family: sans-serif;
+  background-color: white;
+  min-height: 100px;
+  grid-auto-flow: column;
+  grid-gap:20px;
+  padding: 0px 20px 0px 20px;
+  /* grid-template-columns: 20% 20% 20% 20% 20%; */
+  font-size: 25px;
+  .logoitem{
+    display: grid;
+    justify-content: start;
+    align-content: center;
+    margin:10px;
+  }
+  .menuitems{
+    display: grid;
+    justify-content: end;
+    align-content: center;
+    /* grid-template-columns: 20% 20% 20% 20% 20%;
+    grid-area: 1; */
+    grid-auto-flow: column;
+    grid-gap: 20px;
+
+///////////////////////////////
+    // align-items: center;
+    // display: flex;
+    // flex-direction: column;
+    // justify-content: center;
+    // padding: 1em;
+    text-align: center;
+    vertical-align: middle;
+
+    // @media (min-width: 600px) {
+    //     flex-direction: row;
+    //     justify-content: space-between;
+    // }
+    a{
+      text-decoration: none;
+      padding:22px;
+      font-family: sans-serif;
+      font-weight: lighter;
+      color: #333333;
+      position: relative;
+      text-decoration: none;
+      text-transform: uppercase;
+      width: 100%;
+    }
+    a:hover{
+      text-decoration: none;
+      padding:20px;
+      border:2px solid #000000;
+      border-radius: 4px;
+      font-family: sans-serif;
+      font-weight: lighter;
+      color: #000000;
+      position: relative;
+      text-decoration: none;
+      text-transform: uppercase;
+      width: 100%;
+    }
+  }
+/////////////////////////////
+//button-4
+//https://codepen.io/Zoe023/pen/ExjKRmJ
+///////////////////////////
+/////////////////////////////
+//button-4 <a href="https://twitter.com/Dave_Conner" class="btn btn-4"><span>Hover</span></a>
+///////////////////////////
+    .btn-4 {
+      overflow: hidden;
+      position: relative;
+
+      span {
+        // z-index: 20;
+      }
+
+      &:after {
+        background: #000;
+        content: "";
+        height: 225px;
+        width: 65px;
+        left: -250px;
+        opacity: .2;
+        position: absolute;
+        top: -80px;
+        transform: rotate(35deg);
+        transition: all 700ms cubic-bezier(0.19, 1, 0.22, 1);
+
+        // z-index: -10;
+      }
+    }
+
+    .btn-4:hover {
+
+      &:after {
+        left: 150%;
+        transition: all 700ms cubic-bezier(0.19, 1, 0.22, 1);
+      }
+    }
 }
 
 
